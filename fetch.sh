9afb81aac277181b85b8154e961d62686bdc5805
Cob:web Development <75907419+cobwebdev@users.noreply.github.com>
2021-03-23 11:44:51 +0000

    Delete fetch.sh

--- fetch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-printf 'User: ' && whoami
-echo Distro: CelOS 1
-printf 'Kernel: ' && uname -r
-printf 'Uptime: ' && uptime -p | cut -d " " -f2-
-echo Shell: $SHELL
-printf 'Packages: ' && dpkg --list | wc -l
-
