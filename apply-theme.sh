06e6e9e70bbe5cef2dc4136d67cd8032e8b573db
Adam Salt <75907419+whyadamsalt@users.noreply.github.com>
2021-04-13 12:08:56 +0100

    Update apply-theme.sh

--- apply-theme.sh
+++ apply-theme.sh
@@ -7,6 +7,5 @@ if [ ! -f "$HOME/.apply-theme-run" ]; then
   gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
   gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
   gsettings set org.gnome.shell.extensions.user-theme name "Adwaita-shell"
-  flatpak update -y
   touch "$HOME/.apply-theme-run"
 fi
