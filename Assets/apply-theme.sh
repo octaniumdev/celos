e205500ec23bedeaf2fa56a787e93a4b9717f96f
Adam Salt <75907419+whyadamsalt@users.noreply.github.com>
2021-04-14 22:35:58 +0100

    Add files via upload

--- Assets/apply-theme.sh
+++ Assets/apply-theme.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 if [ ! -f "$HOME/.apply-theme-run" ]; then
-  gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
   gnome-extensions disable desktop-icons@csoriano
+  gnome-extensions disable ubuntu-dock@ubuntu.com
   gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-  gnome-extensions enable ubuntu-dock@ubuntu.com
-  gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-  gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
   gsettings set org.gnome.shell.extensions.user-theme name "Adwaita-shell"
   touch "$HOME/.apply-theme-run"
 fi
