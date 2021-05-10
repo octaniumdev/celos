b62fed7cb87eabd94073669d426d7cfaa3a87bcf
Adam Salt <75907419+whyadamsalt@users.noreply.github.com>
2021-05-10 18:52:52 +0100

    Delete apply-theme.sh

--- Assets/apply-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -f "$HOME/.apply-theme-run" ]; then
-  gnome-extensions disable desktop-icons@csoriano
-  gnome-extensions disable ubuntu-dock@ubuntu.com
-  gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
-  gsettings set org.gnome.shell.extensions.user-theme name "Adwaita-shell"
-  touch "$HOME/.apply-theme-run"
-fi
